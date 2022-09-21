3ce317a836df1763f3311296745fbad70676d6f1
Ryan Plitt <ryan.plitt@icloud.com>
2022-09-21 09:36:48 -0600

    resolved manifest

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["RyansPokemonPackage"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.0"),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
