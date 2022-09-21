3a12acebaf29ea75d24b7df60d1774b79dce031f
Ryan Plitt <ryan.plitt@icloud.com>
2022-09-21 09:57:33 -0600

    fixed pikachu

--- Sources/RyansPokemonPackage/RyansPokemonPackage.swift
+++ Sources/RyansPokemonPackage/RyansPokemonPackage.swift
@@ -90,10 +90,10 @@ public enum PokemonGen {
     case original, all
 }
 
-@available(iOS 14.0, *)
+@available(iOS 15.0, *)
 public func getRandomPokemon(gen: PokemonGen = .original) async -> Pokemon {
     let randomNumber = Int.random(in: gen == .original ? 1...150 : 1...905)
-    let url = URL(string: "https://pokeapi.co/api/v2/pokemon/\(String(25))/")!
+    let url = URL(string: "https://pokeapi.co/api/v2/pokemon/\(randomNumber)/")!
     let urlSession = URLSession.shared
     let (data, _) = try! await urlSession.data(from: url)
     let pokemon = try! JSONDecoder().decode(Pokemon.self, from: data)
